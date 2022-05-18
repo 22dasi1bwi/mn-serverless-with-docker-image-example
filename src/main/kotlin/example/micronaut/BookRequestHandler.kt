660211a2ab3aeaf8506e0ebd413951bdfe4ea6a0
Silvio D'Alessandro <silvio.dalessandro@novatec-gmbh.de>
2022-05-18 09:10:09 +0200

    Add bean to be injected

--- src/main/kotlin/example/micronaut/BookRequestHandler.kt
+++ src/main/kotlin/example/micronaut/BookRequestHandler.kt
@@ -1,19 +1,31 @@
 package example.micronaut
 import io.micronaut.core.annotation.Introspected
 import io.micronaut.function.aws.MicronautRequestHandler
+import jakarta.inject.Inject
+import jakarta.inject.Singleton
 import java.util.UUID
 
 @Introspected
 class BookRequestHandler : MicronautRequestHandler<Book?, BookSaved?>() {
 
+    @Inject
+    lateinit var simpleBean: SimpleBean
+
     override fun execute(input: Book?): BookSaved? {
+        val name = simpleBean.getBookName()
         return if (input != null) {
             val bookSaved = BookSaved()
-            bookSaved.name = input.name
+            bookSaved.name = name
             bookSaved.isbn = UUID.randomUUID().toString()
             return bookSaved
         } else {
             null
         }
     }
+}
+
+@Singleton
+class SimpleBean {
+
+    fun getBookName() = "Imagine the book you wanna read"
 }
